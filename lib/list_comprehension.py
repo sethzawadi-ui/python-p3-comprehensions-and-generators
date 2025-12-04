dd27ccc1a162a3daf33132faf4debb7246774c84
sethzawadi-ui <seth.zawadi@student.moringaschool.com>
2025-12-04 16:30:36 +0300

    comprehensiions and generators lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python3
-
 def return_evens(num_list):
-    pass
+    """Return a list of even numbers from num_list."""
+    return [num for num in num_list if num % 2 == 0]
+
 
 def make_exclamation(sentence_list):
-    pass
+    """Add an exclamation mark to the end of each sentence in sentence_list."""
+    return [sentence + "!" for sentence in sentence_list]
